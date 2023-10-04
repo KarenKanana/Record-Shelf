2ac525c741539bd75bb6eaddb090c79baaaaef90
Karen Kanana <kananakaren21@gmail.com>
2023-10-05 00:18:39 +0300

    Add search box, check boxes and button to the Navbar component

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,7 +3,25 @@ import React from 'react';
 const NavBar = () => {
   return (
     <nav className="navbar">
-     Navigation Bar
+     {/* Search Bov */}
+     <div className='search-box'>
+        <input type='text' placeholder='Search'/>
+     </div>
+
+     {/* Checkboxes */}
+     <div className="checkboxes">
+        <label>
+          <input type="checkbox" /> Artist
+        </label>
+        <label>
+          <input type="checkbox" /> Album
+        </label>
+      </div>
+
+      {/* Add New Album Button */}
+      <button type="button" className="add-album-button">
+        Add New Album
+      </button>
     </nav>
   );
 };
