706b6854fccf0b81c3f37e3dc4052b3b12df44eb
Karen Kanana <kananakaren21@gmail.com>
2023-10-04 23:40:58 +0300

    Add AlbumList Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/Navbar/Navbar';
+import AlbumList from './Components/AlbumList/AlbumList';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <AlbumList/>
     </div>
   );
 }
