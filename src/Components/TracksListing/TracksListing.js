13bd2198b1fb13bcbb18a4f48046beb7ca5ed731
Karen Kanana <kananakaren21@gmail.com>
2023-10-06 17:00:59 +0300

    Create TracksListing component

--- src/Components/TracksListing/TracksListing.js
+++ src/Components/TracksListing/TracksListing.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import './TracksListing.css'
+import axios from 'axios';
 
-const TrackListing = ({ tracks }) => {
+const TrackListing = ({ albumId }) => {
+  const [tracks, setTracks] = useState([]);
+
+  useEffect(() => {
+    const fetchTracks = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/tracks"); // Replace with the correct API endpoint for tracks
+        setTracks(response.data);
+      } catch (error) {
+        console.error("Error fetching tracks:", error);
+      }
+    };
+    fetchTracks();
+  }, []);
+
   return (
-    <table className="track-listing">
-     Tracks
-    </table>
+    <div className="tracks-listing">
+      <h2>Tracks</h2>
+      <ul>
+        {tracks.map((track) => (
+          <li key={track.id}>
+            <div className="track-info">
+              <span className="track-number">{track.id}</span>
+              <span className="track-title">{track.name}</span>
+              <span className="track-length">{track.length}</span>
+            </div>
+            <span className="track-artist">{track.artist}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
