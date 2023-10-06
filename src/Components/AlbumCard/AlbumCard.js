80c9de62a24b9c803069c2ac55cfbb6c755e335c
Karen Kanana <kananakaren21@gmail.com>
2023-10-06 18:55:34 +0300

    Make changes to component

--- src/Components/AlbumCard/AlbumCard.js
+++ src/Components/AlbumCard/AlbumCard.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import TracksListing from '../TracksListing/TracksListing';
+import './AlbumCard.css'
 //function that renders album information
 const AlbumCard = ({ album }) => {
   return (
     <div className="album-card">
-      <img src={album.imageUrl} alt={album.name} />
-      <h3>{album.name}</h3>
-      <p>{album.artist}</p>
-      <button>More Info</button>
-      <TracksListing/>
+      <div className="album-details">
+        <img src={album.imageUrl} alt={album.name} />
+        <div>
+          <h3>{album.name}</h3>
+          <p>{album.artist}</p>
+          <button>More Info</button>
+        </div>
+      </div>
+    <div className="tracks-table">
+      <h4>Tracks</h4>
+      <TracksListing albumId={album.id} /> {/* Pass albumId as a prop */}
     </div>
-  );
+  </div>
+);
 };
+    
+
 
 export default AlbumCard;
