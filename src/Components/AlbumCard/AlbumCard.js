123d7010c188c0246c93f9247e93707aa33ff3e1
Karen Kanana <kananakaren21@gmail.com>
2023-10-06 13:52:40 +0300

    Create AlbumCard component

--- src/Components/AlbumCard/AlbumCard.js
+++ src/Components/AlbumCard/AlbumCard.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
+//function that renders album information
 const AlbumCard = ({ album }) => {
   return (
-    <div className="album">
-      {/* Render album cover, title, artist */}
-      {/* <img src={album.coverImage} alt={album.title} /> */}
-      {/* <h2>{album.title}</h2> */}
-      {/* <p>By {album.artist}</p> */}
-      {/* Render TrackListing component with tracks */}
-      I AM A CARD
+    <div className="album-card">
+      <img src={album.imageUrl} alt={album.name} />
+      <h3>{album.name}</h3>
+      <p>{album.artist}</p>
+      <button>More Info</button>
     </div>
   );
 };
