ccca6a19a05834bbec480ecebeaf1b33d44b53bb
Karen Kanana <kananakaren21@gmail.com>
2023-10-06 13:26:10 +0300

    Create a function that renders list of albums

--- src/Components/AlbumList/AlbumList.js
+++ src/Components/AlbumList/AlbumList.js
@@ -1,14 +1,23 @@
 import React from "react";
 import NavBar from "../Navbar/Navbar";
 import AlbumCard from '../AlbumCard/AlbumCard';
-function AlbumList (){
+
+//a function that renders list of albums
+const AlbumList = () => {
     return (
-        <div>
-            
-
-            <AlbumCard/>
-
-        </div>
-    )
-}
+      <div className="album-list">
+        <h2>Albums</h2>
+        <ul>
+          {albums.map((album) => (
+            <li key={album.id}>
+              <img src={album.imageUrl} alt={album.name} />
+              <h3>{album.name}</h3>
+              <p>{album.artist}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  
 export default AlbumList;
