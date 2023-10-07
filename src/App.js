041ac52a675454aab87ea4c70045930fc4a6aa69
Karen Kanana <kananakaren21@gmail.com>
2023-10-07 13:06:52 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import './style.css';
-import NavBar from './Components/Navbar/Navbar';
+// import NavBar from './Components/Navbar/Navbar';
 import AlbumList from './Components/AlbumList/AlbumList';
+import AddAlbumForm from './Components/AddAlbumForm/AddAlbumForm';
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <NavBar/>
-      <AlbumList/>
+    {/* <NavBar /> */}
+        <Routes> 
+          <Route path="/" element={<AlbumList />} />
+          <Route path="/add-album" element={<AddAlbumForm />} />
+        </Routes> 
     </div>
+    </Router>
   );
 }
 
