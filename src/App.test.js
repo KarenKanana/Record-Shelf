567392aa44377e4c99d152c1e12a4c916ce3ca64
Karen Kanana <kananakaren21@gmail.com>
2023-10-09 04:46:08 +0300

    Write test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  it('renders the AlbumList component by default', () => {
+    render(
+      <Router>
+        <App />
+      </Router>
+    );
+
+    // Check if the AlbumList component is rendered
+    expect(screen.getByText('Albums')).toBeInTheDocument();
+  });
+
+  it('renders the AddAlbumForm component when the route is /add-album', () => {
+    render(
+      <Router initialEntries={['/add-album']}>
+        <App />
+      </Router>
+    );
+
+    expect(screen.getByText('Add New Album')).toBeInTheDocument();
+  });
 });
